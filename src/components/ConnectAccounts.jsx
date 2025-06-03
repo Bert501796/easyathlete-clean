a3c28a47b5803419b76f2d68ab8bbef32b7e5b88
Bert501796 <bverschoren@gmail.com>
2025-06-03 09:59:02 +0000

    Uplifted disabled generate schedule button

--- src/components/ConnectAccounts.jsx
+++ src/components/ConnectAccounts.jsx
@@ -125,7 +125,7 @@ const ConnectAccounts = () => {
           className={`mt-6 px-6 py-2 rounded text-white ${
             readyToGenerate ? 'bg-green-600 hover:bg-green-700' : 'bg-gray-400 cursor-not-allowed opacity-50'
           }`}
-          disabled={!readyToGenerate}
+         // disabled={!readyToGenerate}
           onClick={handleGenerateSchedule}
         >
           🚀 Generate Your Training Schedule
